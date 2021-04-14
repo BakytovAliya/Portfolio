042517b7dc340c155e11bb1bdf0a172a1c53651a
BakytovAliya <74789773+BakytovAliya@users.noreply.github.com>
2021-04-15 03:33:08 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -46,45 +46,45 @@ $(function() {
 
 /*JavaScript code*/ 
 window.addEventListener("DOMContentLoaded", function () {
+    
+    var form = document.getElementById("my-form");
+    var status = document.getElementById("status");
 
-  var form = document.getElementById("my-form");
-  var status = document.getElementById("status");
-
-/*Good submit*/
-  function success() {
-    form.reset();
-    status.classList.add("success");
-    status.innerHTML = "Thanks!";
-  }
-
-
-/*Error submit*/
-  function error() {
-    status.classList.add("error");
-    status.innerHTML = "Oops! There was a problem.";
-  }
+    /*Good submit*/
+    function success() {
+        form.reset();
+        status.classList.add("success");
+        status.innerHTML = "Thanks!";
+    }
 
 
-  form.addEventListener("submit", function (ev) {
-    ev.preventDefault();
-    var data = new FormData(form);
-    ajax(form.method, form.action, data, success, error);
-  });
+    /*Error submit*/
+    function error() {
+        status.classList.add("error");
+        status.innerHTML = "Oops! There was a problem.";
+    }
+
+
+    form.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
 });
 
 /*Submit function*/
 function ajax(method, url, data, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      success(xhr.response, xhr.responseType);
-    } else {
-      error(xhr.status, xhr.response, xhr.responseType);
-    }
-  };
-  xhr.send(data);
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
 }
 
