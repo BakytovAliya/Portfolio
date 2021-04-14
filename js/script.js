70c19c0326ab3e3c64bbfdcc025079407be125c9
BakytovAliya <74789773+BakytovAliya@users.noreply.github.com>
2021-04-15 01:36:30 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,87 @@
+/*JQuery code*/
+$(function() {
+    var header = $("#header"),
+        introH = $("#intro").innerHeight(),
+        scrollOffset = $(window).scrollTop();
+    
+    /*Fixed Header*/
+    checkScroll(scrollOffset);
+    
+    $(window).on("scroll", function() {
+        scrollOffset = $(this).scrollTop();
+       
+        checkScroll(scrollOffset);
+    });
+    
+    function checkScroll() {
+        if (scrollOffset >= introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+    
+    /*Smooth Scroll*/
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+        
+        var $this = $(this),
+            blockId = $this.data("scroll"),
+            blockOffset = $(blockId).offset().top;
+        
+        $("#nav a").removeClass("active");
+        $this.addClass("active");
+        
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    
+    });
+    
+});
+
+
+/*JavaScript code*/ 
+window.addEventListener("DOMContentLoaded", function () {
+
+  var form = document.getElementById("my-form");
+  var status = document.getElementById("status");
+
+/*Good submit*/
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks!";
+  }
+
+
+/*Error submit*/
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There was a problem.";
+  }
+
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+/*Submit function*/
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
+
